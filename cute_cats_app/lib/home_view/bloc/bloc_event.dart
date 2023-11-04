2b5ddafcdb7c9b488b9da13e3cb94889ff3c8796
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-04 15:46:38 +0300

    add ✨: bloc_event.dart

--- cute_cats_app/lib/home_view/bloc/bloc_event.dart
+++ cute_cats_app/lib/home_view/bloc/bloc_event.dart
@@ -1,8 +1,28 @@
 part of 'bloc_bloc.dart';
 
-sealed class BlocEvent extends Equatable {
-  const BlocEvent();
+sealed class CatEvent extends Equatable {
+  const CatEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class LoadCatCoutner extends CatEvent {}
+
+class AddCat extends CatEvent {
+  final Cat cat;
+
+  const AddCat(this.cat);
+
+  @override
+  List<Object> get props => [cat];
+}
+
+class RemoveCat extends CatEvent {
+    final Cat cat;
+
+  const RemoveCat(this.cat);
+
+  @override
+  List<Object> get props => [cat];
 }
