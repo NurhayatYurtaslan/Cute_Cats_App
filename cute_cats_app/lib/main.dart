52686cd9c773bf1ad0f5105458fcad6d2bfed28f
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-04 18:19:50 +0300

    update 🆕: home_view.dart

--- cute_cats_app/lib/main.dart
+++ cute_cats_app/lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
 import 'package:cute_cats_app/home_view/bloc/cat_bloc.dart';
-import 'package:cute_cats_app/home_view/widgets/body_widget.dart';
-import 'package:cute_cats_app/home_view/widgets/floatingActionButton_widget.dart';
+import 'package:cute_cats_app/home_view/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,37 +30,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final Random random = Random();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cats App', style: TextStyle(fontSize: 30)),
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[Colors.black, Colors.purpleAccent])),
-        ),
-      ),
-      body: bodyWidget(random: random),
-      floatingActionButton: const floatingActionButton(),
-    );
-  }
-}
-
-
-
