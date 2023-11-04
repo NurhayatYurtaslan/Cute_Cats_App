8c6151cd1d39d23bbedb79951976840e5f571c38
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-04 15:46:26 +0300

    add ✨: bloc_bloc.dart

--- cute_cats_app/lib/home_view/bloc/bloc_bloc.dart
+++ cute_cats_app/lib/home_view/bloc/bloc_bloc.dart
@@ -1,13 +1,28 @@
+// ignore: depend_on_referenced_packages
+import 'dart:ffi';
 import 'package:bloc/bloc.dart';
+import 'package:cute_cats_app/home_view/cat_view.dart';
 import 'package:equatable/equatable.dart';
-
 part 'bloc_event.dart';
 part 'bloc_state.dart';
 
-class BlocBloc extends Bloc<BlocEvent, BlocState> {
-  BlocBloc() : super(BlocInitial()) {
-    on<BlocEvent>((event, emit) {
-      // TODO: implement event handler
+class CatBloc extends Bloc<CatEvent, CatState> {
+  CatBloc() : super(CatInitial()) {
+    on<LoadCatCoutner>((event, emit) async {
+      await Future<dynamic>.delayed(const Duration(seconds: 1));
+      emit(const CatLoaded(cats: <Cat>[]));
+    });
+    on<AddCat>((event, emit) {
+      if (state is CatLoaded) {
+        final state = this.state as CatLoaded;
+        emit(CatLoaded(cats: List.from(state.cats)..add(event.cat)));
+      }
+    });
+    on<RemoveCat>((event, emit) {
+      if (state is CatLoaded) {
+        final state = this.state as CatLoaded;
+        emit(CatLoaded(cats: List.from(state.cats)..remove(event.cat)));
+      }
     });
   }
 }
